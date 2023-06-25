0e1dee0b37457d0f2da3151a4c676a6a2c0bbd82
Kueanth <kueanth@gmail.com>
2023-06-25 23:22:33 +0300

    Полностью исправленное освещение противников

--- Assets/Objects/Enemy/Enemy 0/Scripts/EnemyLight.cs
+++ Assets/Objects/Enemy/Enemy 0/Scripts/EnemyLight.cs
@@ -3,4 +3,20 @@ using UnityEngine;
 
 public class EnemyLight : MonoBehaviour
 {
+    [SerializeField] private Material material;
+
+    public void StartGame()
+    {
+        StartCoroutine(EditCoroutine(material));
+    }
+
+    IEnumerator EditCoroutine(Material material)
+    {
+        for(float i = 1f; i >= -1f; i -= 0.1f)
+        {
+
+            material.SetColor("_Color", material.color * i);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 }
