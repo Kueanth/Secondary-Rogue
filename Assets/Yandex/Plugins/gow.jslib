ba0d2814a3e88fd1790372e93ecdbdebae7b6ffb
Паша Веренич <kueanth@gmail.com>
2023-10-29 04:35:22 +0300

    Интеграция Яндекс СДК

---
+++ Assets/Yandex/Plugins/gow.jslib
@@ -0,0 +1,9 @@
+// Ебал рот того, кто это читает, чтоб у тебя мать сдохла нахуй
+
+mergeInto(LibraryManager.library, {
+
+  GetDataPlayer: function () {
+    myGameInstance.SendMessage('Initialization Yandex SDK', 'GetData', player.getName());
+  },
+
+  });
---
+++ Assets/Yandex/Scripts/InitYandex.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using System.Runtime.InteropServices;
+
+public class InitYandex : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _name;
+
+    [DllImport("__Internal")]
+    private static extern void GetDataPlayer();
+
+    public void Start()
+    {
+        GetDataPlayer();
+    }
+
+    public void GetData(string name)
+    {
+        _name.text = name;
+    }
+}
